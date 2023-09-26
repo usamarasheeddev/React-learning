ed26b5eae8318397614ad1181846ed1efa798f7a
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2023-09-26 17:58:36 +0500

    react learing app

---
+++ learning-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Home from './Home/Home'
+export default function App() {
+  return (
+    <div>
+      helo
+
+      <Home/>
+    </div>
+  )
+}
---
+++ learning-react-app/src/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      thi is  Home component
+    </div>
+  )
+}
